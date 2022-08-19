e3e5f8a6d13720427e5be4b01eac6bcbb05bac40
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-19 10:09:39 -0600

    Manipulando el DOM

--- script.js
+++ script.js
@@ -3,8 +3,22 @@
 const title = document.querySelector('h1');
 const p = document.querySelector('p');
 const parrafito = document.querySelector('.parrafo');
-const input = document.querySelector('#pid');
+const parrafoConId = document.querySelector('#pid');
+const input = document.querySelector('input');
 
 console.log(title.outerHTML);
 
 console.log({title, p, parrafito, input});
+
+console.log(title.getAttribute('class'));
+title.setAttribute('class', 'title');
+
+title.classList.add('title');
+title.classList.remove('Titulo');
+
+input.value = "456"
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://static.platzi.com/media/logotipo-platzi.png');
+
+parrafoConId.append(img);
