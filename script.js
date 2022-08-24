e60a8c9a4f0949e7e02d9ce186d548e69f870000
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-24 11:42:02 -0600

    Add Event listener

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
-const btn = document.querySelector("#calculoboton");
-const pResult = document.querySelector(".result");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
 
-function btnOnClick() {
-	const resultadoSumaInputs =
-		parseFloat(input1.value) + parseFloat(input2.value);
-	pResult.innerText = "Resultado: " + resultadoSumaInputs;
+form.addEventListener("submit", sumarInputValues);
+
+function sumarInputValues(event) {
+	console.log({ event });
+	event.preventDefault();
+	const sumaInputs = parseFloat(input1.value) + parseFloat(input2.value);
+	pResult.innerText = "Resultado: " + sumaInputs;
 }
